db4cadacbbf8527b8c581e96abded82d7e78c0cd
yyclvps <36289902+yyclvps@users.noreply.github.com>
2024-01-26 15:41:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.25.1/g' package/base-files/files/bin/config_generate
+
+sed -i 's/PKG_USE_MIPS16/PKG_BUILD_FLAGS:=no-mips16\nPKG_USE_MIPS16/' feeds/helloworld/v2ray-plugin/Makefile
